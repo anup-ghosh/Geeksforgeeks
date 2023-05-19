1621cf2f5417665a7d02e7ef4ee1acf02a5139e5
Anup Ghosh <anupbsmrstu@gmail.com>
2023-05-19 08:32:45 +0600

    Cpp

--- Find k-th smallest element in given n ranges.cpp
+++ Find k-th smallest element in given n ranges.cpp
@@ -1,6 +1,6 @@
 class Solution{
     public:
     vector<int>kthSmallestNum(int n, vector<vector<int>>&range, int q, vector<int>queries){
-        //Write your code here
+        Write your code here
     } 
 };
